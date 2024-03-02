5edc5a956f0094485fac2340fc0c2674aea89df9
Franklin Ebi <esendufranklinebi@gmail.com>
2024-03-02 12:23:17 +0100

    finished third section

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import Marketimage from "../public/marketimage.png"
 import Secureimage from "../public/securetrade.png"
 import { useState } from "react"
 import { ReactNode } from "react";
+import { Button } from "@/components/ui/button";
 
 
 interface AnimatedComponentProps {
@@ -82,10 +83,7 @@ export default function App(){
   className="w-64 h-64 rounded-full bg-gradient-to-r from-neutral-800 to-gray-100 dark:to-gray-500 absolute bottom-3 md:bottom-1 -left-40 z-40"></motion.div>
 </section>
 
-
-
-
-<section className="h-screen overflow-x-hidden relative md:px-10 mt-10 py-4 dark:bg-black md:flex md:justify-between">
+<section className="md:flex md:justify-between md:px-5 mt-8">
 
    
 
@@ -136,25 +134,146 @@ export default function App(){
 
   </div>
   <Image src={Secureimage} className="mt-3 md:mt-0  z-50" alt=""/>
-  <motion.div
+   <motion.div
   initial={
     {
       x:50
     }
   }
   animate={{x:0}}
-  transition={{duration:0.7}} className="w-64 h-64 rounded-full bg-gradient-to-r from-gray-200 to-pink-200 absolute  -right-40 -z-50"></motion.div>
+  transition={{duration:0.7}} className="w-64 h-64 rounded-full bg-gradient-to-r from-gray-200 to-pink-200 absolute  -right-40  -z-50"></motion.div>
 
 
-</section>
+ </section>
 
-<section className="md:mt-20 md:pl-10">
+
+
+<section className="h-fit md:mb-10 mt-20 md:pl-10 md:flex md:justify-between">
 <AnimatedComponent>
     <h2 className="text-[2.5rem] text-center font-bold text-[#8670FC]  md:w-[22.8rem] leading-[48px] px-2 md:px-0 mb-4 md:text-left">
     Join the Crypto Trading Revolution Today!
       </h2>
+    </AnimatedComponent> 
+    <div>
+    <AnimatedComponent>
+      <p className="text-center md:text-left md:w-[42vw] md:mt-5  md:text-xl text-gray-700 mb-20 dark:text-gray-300">A user friendly platform that enables trade and investment and helps you grow your wealth with ease.</p>
     </AnimatedComponent>
+
+    <div className="px-3 flex flex-col gap-y-3">
+    <AnimatedComponent>
+      <p className="text-4xl text-[#8670FC] font-bold">50%</p>
+      <p className="text-sm">Active Traders:10,000+</p>
+    </AnimatedComponent>
+    <AnimatedComponent>
+      <p className="text-4xl text-[#8670FC] font-bold">50%</p>
+      <p className="text-sm">Transaction volume:  $1 million +</p>
+    </AnimatedComponent>
+
+
+    </div>
+      
+    </div>
+    
+    
+   
+
 </section>
+<section id="market" className="py-10 md:px-0 mt-20 text-[#8670FC]">
+              <h3 className="text-center font-bold text-2xl mb-10 md:text-3xl">
+                ACCOUNT TYPES
+              </h3>
+              <div className="md:flex md:gap-x-3 px-2">
+
+
+              <div className="text-center  border py-9 text-sm rounded-md mb-7   md:w-2/6 dark:border-white">
+                <p className="font-semibold">STARTER</p>
+                <p className="font-bold text-2xl mb-3 ">1000 $/EUR</p>
+                <ul className=" mb-6 text-base text-black dark:text-[#8670FC] md:text-lg">
+                  <li className="mb-3">
+                    Trading Instruments: 36 currency pairs, metals,
+                    cryptocurrencies
+                  </li>
+                  <li className="mb-3">Spread: Floating from 1.3 pips</li>
+                  <li className="mb-3">Maximum Leverage: 1:2000</li>
+                  <li className="mb-3">Return on Investment:10%</li>
+                  <li>Deposit Bonuses: All Offers</li>
+                </ul>
+
+                <Button className="border-2 dark:bg-black dark:hover:bg-[#8670FC] dark:hover:text-white  hover:bg-[#8670FC] hover:text-white font-semibold md:mt-6 dark:text-[#8670FC]">
+                  Choose Plan
+                </Button>
+              </div>
+              <div className="text-center border px-4 py-9 text-sm rounded-md mb-7 md:w-2/6  dark:border-white">
+                <p className="font-semibold ">BASIC</p>
+                <p className="font-bold text-2xl mb-3 "> 5000 $/EUR</p>
+                <ul className="mb-6 text-base text-black dark:text-[#8670FC] md:text-lg">
+                  <li className="mb-3">
+                    Trading Instruments: 36 currency pairs,CbFDs on US
+                    stocks,CFDs on indices,CFDs on oil, cryptocurrencies
+                  </li>
+                  <li className="mb-3">Spread: Floating from 1.3 pips</li>
+                  <li className="mb-3">Maximum Leverage: 1:2000</li>
+                  <li className="mb-3">Return on Investment:15%</li>
+                  <li className="mb-3">Deposit Bonuses: All Offers</li>
+                </ul>
+
+                <Button className="border-2 dark:hover:bg-[#8670FC] dark:hover:text-white dark:bg-black hover:bg-[#8670FC] hover:text-white  font-semibold md:mt-6 text-[#8670FC]">
+                  Choose Plan
+                </Button>
+              </div>
+              <div className="text-center border px-4 py-9 text-sm rounded-lg mb-7 md:w-2/6  dark:border-white">
+                <p className="font-semibold">PREMIUM</p>
+                <p className="font-bold   mb-3 text-2xl">10,000 $/EUR</p>
+                <ul className="mb-6 text-base text-black dark:text-[#8670FC] md:text-lg">
+                  <li className="mb-3">
+                    Trading Instruments: 36 currency pairs,CFDs on US
+                    stocks,CFDs on indices,CFDs on oil, cryptocurrencies
+                  </li>
+                  <li className="mb-3">Spread: Floating from 1.3 pips</li>
+                  <li className="mb-3">Maximum Leverage: 1:500</li>
+                  <li className="mb-3">Return on Investment:20%</li>
+                  <li className="mb-3">Deposit Bonuses: All Offers</li>
+                  <li className="mb-3">Loyalty Bonuses: All Offers</li>
+                </ul>
+
+                <Button className="border-2 dark:hover:bg-[#8670FC] dark:hover:text-white hover:bg-[#8670FC] hover:text-white text-[#8670FC] dark:bg-black font-semibold">
+                  Choose Plan
+                </Button>
+              </div>
+              </div>
+
+            </section>
+
+            <section className="px-5 py-4 md:mb-20 md:flex md:justify-between text-center md:text-left ">
+              
+    <AnimatedComponent>
+    <h2 className="text-[2.5rem] text-center font-bold text-[#8670FC]  md:w-[22.8rem] leading-[48px] px-2 md:px-0 mb-4 md:text-left">
+    Start Trading Today
+      </h2>
+    </AnimatedComponent>
+    <AnimatedComponent>
+      <p className="text-center md:text-left md:w-[42vw] md:mt-5  md:text-xl text-gray-700 mb-20 md:mb-0 dark:text-gray-300">Join our platform and start enjoying the benefits from our user friendly interface to our ease of service</p>
+
+      <Button className="border-2 dark:hover:bg-[#8670FC] dark:hover:text-white hover:bg-[#8670FC] hover:text-white text-[#8670FC] dark:bg-black font-semibold">
+        Sign up
+      </Button>
+    </AnimatedComponent>
+
+            </section>
+
+
+            <section className="mt-20">
+                         
+    <AnimatedComponent>
+    <h2 className="text-[2.5rem] text-center font-bold text-[#8670FC]  md:w-[22.8rem] leading-[48px] px-2 md:px-0 mb-4">
+    Get in Touch
+      </h2>
+    </AnimatedComponent>
+              
+
+            </section>
+
+
 
 
 </main>
