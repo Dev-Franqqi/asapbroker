c90faa9e93ef4c75eeeec5e1b708ac75801e6d1d
Franklin Ebi <esendufranklinebi@gmail.com>
2024-03-02 01:14:41 +0100

    Finished first section of new design

--- app/mycomps/Navbar.tsx
+++ app/mycomps/Navbar.tsx
@@ -1,1 +1,81 @@
+"use client"
+import Image from "next/image"
+import Link from "next/link";
+import Logo from "../../public/elitlogo.png"
+import { Poppins } from "next/font/google";
+import { Button } from "@/components/ui/button";
+import { Dispatch, useState,SetStateAction } from "react";
+const poppins = Poppins({ weight:"700" ,subsets: ["latin"] });
 
+type Props ={
+    darkMode:boolean,
+    setdarkMode: Dispatch<SetStateAction<boolean>>
+}
+export default function Navbar({setdarkMode,darkMode}:Props){
+    
+
+    return(
+        <>
+        <nav className="px-4 py-2 flex justify-between">
+            <div className="flex mt-3 ">
+            <Image src={Logo} alt="" className="rounded-md"  />
+            <header className={`${poppins.className} mt-2`}>
+                ELITE TRADING HUB
+            </header>
+                
+            </div>
+            <div className="md:w-2/5 mt-4 flex space-x-2 ">
+
+
+            <ul className="pt-2 w-3/5 md:flex space-x-9 font-medium text-sm hidden  ">
+                
+                <li className="hover:underline ">
+                    <Link href="/">About</Link>
+                </li>
+                <li className="hover:underline ">
+                <Link href="/">Pricing</Link>
+
+                </li>
+                    <li className="hover:underline ">
+                    <Link href="/">Contact</Link>
+
+
+                    </li>
+
+            </ul>
+
+
+
+            <div className="flex space-x-2">
+                <Button className="text-black border dark:bg-[#8670FC] hover:bg-[#8670FC] hover:text-white border-[#8670FC] text-center hidden md:block">Sign up</Button>
+                <Button className="bg-black hover:text-white dark:border dark:border-[#8670FC] hover:bg-[#8670FC]  text-white">Log in</Button>
+                
+            {!darkMode &&  <svg
+    onClick={() => setdarkMode(true)}
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6 mt-2 md:mt-[0.8rem] md:text-black md:border-0 hover:text-[#8670FC]"
+>
+    <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+    />
+</svg>}
+
+                
+               
+
+
+            </div>
+            </div>
+
+
+                
+            
+            </nav></>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,71 @@
-export default function app(){
-   
+"use client"
+import {motion} from "framer-motion"
+import Navbar from "./mycomps/Navbar"
+import Image from "next/image"
+import Marketimage from "../public/marketimage.png"
+import { useState } from "react"
+export default function App(){
+  const [darkMode,setDarkMode] = useState<boolean>(false)
+   console.log(darkMode)
 
   return(
+    <>
 
-      <>
-          
+      <div className={darkMode?"dark bg-black text-white":""}>
+          <Navbar darkMode={darkMode} setdarkMode={setDarkMode}/>
+
+          <main className="">
+
+<section className="h-screen text-center md:text-left  flex flex-col md:flex-row md:items-center mt-10 justify-center md:px-10">
+
+  <motion.div className="z-50 md:order-1 md:mr-auto md:ml-0 relative md:top-0 px-3 top-40"
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5,delay:1 }}>
+    <h1 className="text-[2.5rem] font-bold text-[#8670FC]  md:w-[22.8rem] leading-[48px] px-2 md:px-0 mb-4">
+      Trade Crypto and Invest with Confidence
+    </h1>
+    <p className="md:w-[42vw]  md:text-xl text-gray-700 mb-20 dark:text-gray-300">
+      Discover the world of cryptocurrency trading and investment with our user-friendly platform. Start today and take advantage of the opportunities in the crypto market.
+    </p>
+  </motion.div>
+
+
+    <motion.div initial={{opacity:0,y:-20}} animate={{opacity:1,y:0}} transition={{duration:0.6 ,delay:2}} className="md:order-3  md:ml-auto mx-2 mt-40 md:-mt-10 z-50">
+  <Image  src={Marketimage} alt="" className="" />
+
+
+    </motion.div>
+
+  <motion.div initial={
+    {
+      x:-50
+    }
+  }
+  animate={{x:0}}
+  transition={{duration:0.7}}
+  
+  className="w-64 h-64 rounded-full bg-gradient-to-r from-neutral-800 to-gray-100 dark:to-gray-500 absolute bottom-3 md:bottom-1 -left-40 z-40"></motion.div>
+  {/* <div className="w-64 h-64 rounded-full bg-gradient-to-r from-gray-200 to-black absolute bottom-1 -right-32 z-40"></div> */}
+</section>
+
+
+
+
+<section className="screen">
+
+  <div>
+    <p className="font-semibold text-sm">Secure</p>
+  </div>
+
+</section>
+</main>
+
       
       
-      </>
+      </div>
+    </>
+
 
   )
 }
